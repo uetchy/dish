af3cfec864b7b4e6fd551510e2c2608eb66dd8f1
Yasuaki Uechi <uetchy@randompaper.co>
2014-07-12 16:13:01 +0900

    Update for Faraday

--- app/controllers/maps_controller.rb
+++ app/controllers/maps_controller.rb
@@ -1,15 +1,19 @@
 class MapsController < ApplicationController
+  include ApplicationHelper
+
+  respond_to :json
+
   def index
   end
 
   def search(lat, lng, radius)
-    data = Faraday.get "https://api.foursquare.com/v2/venues/explore?client_id=#{ENV['CLIENT_ID']}&client_secret=#{ENV['CLIENT_SECRET']}&v=20130815&ll=#{[lat, lng].join(',')}&radius=#{radius}&venuePhotos=1&section=food"
-    response = JSON.parse(data.body)['response']['groups'][0]['items']
-
-    # response = client.search_venues(ll: [lat, lng].join(','), radius: radius)
-
-    respond_to do |format|
-      format.json {render :json => response.to_json}
-    end
+    data = get4sq "venues/explore", {
+      ll: [lat, lng].join(','),
+      radius: radius,
+      venuePhotos:1,
+      section: 'food'
+    }
+    response = data['response'].present? ? data['response']['groups'][0]['items'] : []
+    respond_with response
   end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  def get4sq(path, args)
+    data = Faraday.get URI.join("https://api.foursquare.com/v2/", path), {
+      client_id: ENV['CLIENT_ID'],
+      client_secret: ENV['CLIENT_SECRET'],
+      v: 20130815
+    }.merge(args)
+    JSON.parse(data.body)
+  end
 end
